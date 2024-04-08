9c269d9aca39c4902edc514b1404c563f1942410
Moosung-Kang <bbibbi4808@naver.com>
2024-04-08 11:09:58 +0900

    240408_1

---
+++ 스터디/3주차 [삼성 기출 1]/P14891 톱니바퀴.py
@@ -0,0 +1,15 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+def rotate(idx, dir) :
+    print("")
+
+
+
+if __name__ == "__main__":
+    gears = [deque(map(int, input().rstrip())) for _ in range(4)]
+    k = int(input())
+    info = [tuple(map(int, input().split())) for _ in range(k)]
+    print(gears, k, info)
