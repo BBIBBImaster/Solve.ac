8c69812c1195e3a73a24fbe330300990c4119816
강무성 <bbibbi4808@naver.com>
2024-08-04 01:08:45 +0900

    240804

---
+++ 스터디/9주차/P2164 카드2.py
@@ -0,0 +1,7 @@
+
+
+
+if __name__ == "__main__" :
+  import sys
+  input = sys.stdin.readline
+  n = int(input())
--- 스터디/9주차/P9568 덩치.py
+++ 스터디/9주차/P9568 덩치.py
@@ -1,5 +1,24 @@
+def find_ranking(n, members):
+    ranking_list = []
 
+    for i in range(n):
+        rank = 1
+        for j in range(n):
+            if i != j:
+                if (int(members[i][0]) < int(members[j][0])) and (int(members[i][1]) < int(members[j][1])):
+                    rank += 1
+        ranking_list.append(rank)
 
-if __name__ == "__main__" : 
-  import sys
-  input = sys.stdin.readline
+    answer = ' '.join(map(str, ranking_list))
+    return answer
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.readline
+    n = int(input())
+    members = []
+    for _ in range(n):
+        member = list(map(int, input().split()))
+        members.append(member)
+
+    print(find_ranking(n, members))
