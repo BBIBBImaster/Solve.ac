2085606c7c3620fc48bac13f48ac26cabb2c50f6
강무성 <bbibbi4808@naver.com>
2024-08-03 16:23:46 +0900

    240803

--- 스터디/9주차/P20125 쿠키의 신체측정.py
+++ 스터디/9주차/P20125 쿠키의 신체측정.py
@@ -11,10 +11,44 @@ def find_length(n, graph):
             break
     
     left_arm = 0
+    left_arm_candidate = graph[i+1][0: j]
+    for k in range(len(left_arm_candidate)) :
+        if left_arm_candidate[k] == "*" :
+            left_arm += 1
+  
     right_arm = 0
+    right_arm_candidate = graph[i+1][j+1 : n]
+    for k in range(len(right_arm_candidate)) :
+        if right_arm_candidate[k] == "*" :
+            right_arm += 1
+
     waist = 0
+    waist_candidate = graph[i + 2][j]
+    while waist_candidate == '*' :
+        waist += 1
+        if i + 2 + waist < n:
+            waist_candidate = graph[i + 2 + waist][j]
+        else:
+            break
+  
+        
     left_leg = 0
+    left_leg_candidate = graph[i + 2 + waist][j-1]
+    while left_leg_candidate == '*' :
+        left_leg += 1
+        if i + 2 + waist + left_leg < n :
+            left_leg_candidate = graph[i+ 2 + waist + left_leg][j-1]
+        else :
+            break
+
     right_leg = 0
+    right_leg_candidate = graph[i + 2 + waist][j+1]
+    while right_leg_candidate == '*' :
+        right_leg += 1
+        if i + 2 + waist + right_leg < n :
+            right_leg_candidate = graph[i+ 2 + waist + right_leg][j+1]
+        else :
+            break
 
     return heart_x, heart_y, left_arm, right_arm, waist, left_leg, right_leg
 
