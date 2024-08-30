5e0e0226013c6806659e4dafc9ab238c6fc86251
강무성 <bbibbi4808@naver.com>
2024-08-30 16:16:51 +0900

    wip

---
+++ 알고리즘 분류 별/자료 구조/P18258 큐2.py
@@ -0,0 +1,43 @@
+import sys
+from collections import deque
+
+if __name__ == "__main__" :
+  input = sys.stdin.readline
+  queue = deque()
+
+  n = int(input())
+
+  for _ in range(n) :
+    command = input().strip()
+    
+    if command.startswith("push") :
+      _, num = command.split(" ")
+      queue.append(num)
+
+    elif command == "pop" :
+      if queue :
+        print(queue.popleft())
+      else :
+        print(-1)
+
+    elif command == "size" :
+      print(len(queue))
+
+    elif command == "empty" :
+      if queue :
+        print(0)
+      else :
+        print(1)
+
+    elif command == "front" :
+      if queue :
+        print(queue[0])
+      else :
+        print(-1)
+
+    # command = "back"
+    else :
+      if queue :
+        print(queue[-1])
+      else :
+        print(-1)
--- 알고리즘 분류 별/자료 구조/P1874 스택 수열2.py
+++ 알고리즘 분류 별/자료 구조/P1874 스택 수열2.py
@@ -1,6 +1,9 @@
+# 스택은 자료를 넣는 (push) 입구와 자료를 뽑는 (pop) 입구가 같아
+
 import sys
 
 
 if __name__ == "__main__" :
   input = sys.stdin.readline
-    
+  n = int(input())
+  
