e14fb19b47a7fb9e95832b05189b428838a02e9b
Moosung Kang <bbibbi4808@naver.com>
2024-08-30 16:29:23 +0900

    240830

--- 알고리즘 분류 별/자료 구조/P9093 문자열 뒤집기.py
+++ 알고리즘 분류 별/자료 구조/P9093 문자열 뒤집기.py
@@ -3,3 +3,9 @@ import sys
 if __name__ == "__main__" :
   input = sys.stdin.readline
   t = int(input())
+
+  for _ in range(t) :
+    sentence = input().rstrip().split(" ")
+    num_word = len(sentence)
+    for i in range(num_word) :
+      print(sentence[i][::-1], end = " ")
