697c134805cf21ba7cda990cb0738ed514ecb353
Moosung Kang <bbibbi4808@naver.com>
2024-08-30 18:32:13 +0900

    wip

--- 알고리즘 분류 별/자료 구조/P1874 스택 수열2.py
+++ 알고리즘 분류 별/자료 구조/P1874 스택 수열2.py
@@ -1,9 +1,38 @@
-# 스택은 자료를 넣는 (push) 입구와 자료를 뽑는 (pop) 입구가 같아
-
 import sys
 
+def find_answer(target_list) :
+  stack = []
+  result = []
+  current = 1
+
+  for num in target_list :
+    while current <= num :
+      stack.append(current)
+      current += 1
+      result.append("+")
+      
+    if stack[-1] == num :
+      stack.pop()
+      result.append("-")
+
+    else :
+      return "NO"
+
+  return result
 
 if __name__ == "__main__" :
   input = sys.stdin.readline
   n = int(input())
-  
+  target_list = []
+
+  for _ in range(n) :
+    num = int(input())
+    target_list.append(num)
+
+  result = find_answer(target_list)
+
+  if result == "NO":
+     print(result)
+  else:
+    for operation in result:
+      print(operation)
